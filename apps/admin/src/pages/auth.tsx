e290facd919911dafb4950b4fa74e1e0822d8fe9
Ebonian <tanadon.santisan@gmail.com>
2023-06-20 17:42:53 +0700

    chore: ui for auth page

--- apps/admin/src/pages/auth.tsx
+++ apps/admin/src/pages/auth.tsx
@@ -1,7 +1,18 @@
 import { NextPage } from 'next';
 
 const Auth: NextPage = () => {
-    return <div>Auth</div>;
+    return (
+        <main className="grid place-content-center h-screen">
+            <div className="border-neutral-200 border rounded-xl p-6 space-y-10 w-80">
+                <h1 className="font-bold">เข้าสู่ระบบ Vishnu21 Admin</h1>
+                <div className="flex justify-end">
+                    <button className="font-semibold text-white px-4 py-2 bg-neutral-800 rounded-lg shadow-md">
+                        เข้าสู่ระบบด้วย CUNET
+                    </button>
+                </div>
+            </div>
+        </main>
+    );
 };
 
 export default Auth;
