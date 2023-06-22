211bd0a56d15a4723f93682b15cc1d5d942d6392
Zenith <sqoulo11@gmail.com>
2023-06-22 22:12:27 +0700

    feat: change filter button and update top selector

--- apps/liff/src/pages/club/components/clubcard.tsx
+++ apps/liff/src/pages/club/components/clubcard.tsx
@@ -1,8 +1,8 @@
 import { AiFillHeart } from "react-icons/ai";
 
-const ClubCard = ({ img = <div className='flex bg-gray-500 h-[72px] w-[72px] rounded-[20px] place-self-center' />, name = "Card name", isStamped = false, isFavorite = false }) => {
+const ClubCard = ({ img = <div className='flex bg-gray-500 h-[72px] w-[72px] rounded-[20px] place-self-center' />, name = "Card name", isStamped = false, isFavorite = false, currentMode }) => {
     const filter = (isStamped) ? <div /> : <div className='flex absolute bg-[#3B0D8D]/[70%] w-full h-full rounded-[20px]' />
-    const icon_heart = (isFavorite) ? <AiFillHeart className="absolute h-[30px] w-[30px] top-0 right-0" color="red" /> : <div />
+    const icon_heart = (currentMode == "Favorite" && isFavorite) ? <AiFillHeart className="absolute h-[30px] w-[30px] top-0 right-0" color="red" /> : <div />
     // TODO: change img,name according to input
     return (
         <div className='flex relative items-center justify-center'>
--- apps/liff/src/pages/club/index.tsx
+++ apps/liff/src/pages/club/index.tsx
@@ -2,20 +2,26 @@ import * as React from 'react';
 import { useState } from 'react';
 import ClubCard from './components/clubcard';
 import { AiFillHeart } from 'react-icons/ai';
-import { FaStamp , FaSlash } from 'react-icons/fa';
+import { FaStamp, FaSlash, FaFilter } from 'react-icons/fa';
 
 
 const Club = () => {
-    const [filterFavorite, setFilterFavorite] = useState(false);
-    // 0 => all , 1 => filter stamped , 2 => filter not stamped
-    const [filterStamp, setFilterStamp] = useState(0);
+    // 0 => all , 1 => filter , 2 => not filter
+    const [filterState, setFilterState] = useState(0);
+    const [currentMode, setCurrentMode] = useState("Stamp");
+    const handleFilter = () => {
+        const totalState = (currentMode == "Stamp") ? 3 : 2;
+        setFilterState((filterState + 1) % totalState)
+    }
+    const handleChangeMode = () => {
+        setFilterState(0);
+        (currentMode == "Stamp") ? setCurrentMode("Favorite") : setCurrentMode("Stamp");
+    }
+    const filterSlash = (filterState == 2 && currentMode == "Stamp") ? <FaSlash className='w-[30px] h-[30px] absolute top-0' color='red' /> : <div />;
+    const filterColor = (filterState == 1) ? 'green' : '#D9D9D9';
 
-    const toggleFavorite = () => {setFilterFavorite(!filterFavorite)};
-    const toggleStamp = () => {setFilterStamp((filterStamp+1)%3)};
-
-    const favoriteColor = (filterFavorite) ? 'red' : '#dedddf';
-    const stampColor = (filterStamp == 1) ? 'green' : '#dedddf';
-    const stampSlash = (filterStamp == 2) ? <FaSlash className='w-[30px] h-[30px] absolute top-0' color='red'/> : <div />;
+    // All , Academic , Sport , Art , CSR
+    const [selection, setSelection] = useState("All");
 
     return (
         // TODO: change background image
@@ -32,19 +38,19 @@ const Club = () => {
                     </h1>
                     {/* TODO: set onclick */}
                     <div className='flex flex-row gap-4 mt-[12px] items-center justify-center'>
-                        <h1 className='font-semibold text-white text-[14px]'>
+                        <h1 className={`font-semibold text-white text-[14px] ${(selection == "All") ? "" : "opacity-60"}`} onClick={() => setSelection("All")}>
                             ทั้งหมด
                         </h1>
-                        <h1 className='font-semibold text-white/[0.6] text-[14px]'>
+                        <h1 className={`font-semibold text-white text-[14px] ${(selection == "Academic") ? "" : "opacity-60"}`} onClick={() => setSelection("Academic")}>
                             วิชาการ
                         </h1>
-                        <h1 className='font-semibold text-white/[0.6] text-[14px]'>
+                        <h1 className={`font-semibold text-white text-[14px] ${(selection == "Sport") ? "" : "opacity-60"}`} onClick={() => setSelection("Sport")}>
                             กีฬา
                         </h1>
-                        <h1 className='font-semibold text-white/[0.6] text-[14px]'>
+                        <h1 className={`font-semibold text-white text-[14px] ${(selection == "Art") ? "" : "opacity-60"}`} onClick={() => setSelection("Art")}>
                             ศิลปะวัฒนธรรม
                         </h1>
-                        <h1 className='font-semibold text-white/[0.6] text-[14px]'>
+                        <h1 className={`font-semibold text-white text-[14px] ${(selection == "CSR") ? "" : "opacity-60"}`} onClick={() => setSelection("CSR")}>
                             CSR
                         </h1>
                     </div>
@@ -53,36 +59,24 @@ const Club = () => {
                 {/* club list */}
                 {/* TODO: create component according to data */}
                 <div className='grid grid-flow-row grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 mt-[25px] w-full gap-[20px]'>
-                    <ClubCard ></ClubCard>
-                    <ClubCard isStamped={true}></ClubCard>
-                    <ClubCard isFavorite={true} isStamped={true}></ClubCard>
-                    <ClubCard ></ClubCard>
-                    <ClubCard isStamped={true}></ClubCard>
-                    <ClubCard isFavorite={true} isStamped={true}></ClubCard>
-                    <ClubCard ></ClubCard>
-                    <ClubCard isStamped={true}></ClubCard>
-                    <ClubCard isFavorite={true} isStamped={true}></ClubCard>
-                    <ClubCard ></ClubCard>
-                    <ClubCard isStamped={true}></ClubCard>
-                    <ClubCard isFavorite={true} isStamped={true}></ClubCard>
-                    <ClubCard ></ClubCard>
-                    <ClubCard isStamped={true}></ClubCard>
-                    <ClubCard isFavorite={true} isStamped={true}></ClubCard>
-                    <ClubCard ></ClubCard>
-                    <ClubCard isStamped={true}></ClubCard>
-                    <ClubCard isFavorite={true} isStamped={true}></ClubCard>
-                    <ClubCard ></ClubCard>
-                    <ClubCard isStamped={true}></ClubCard>
-                    <ClubCard isFavorite={true} isStamped={true}></ClubCard>
-                    <ClubCard ></ClubCard>
-                    <ClubCard isStamped={true}></ClubCard>
-                    <ClubCard isFavorite={true} isStamped={true}></ClubCard>
-                    <ClubCard ></ClubCard>
-                    <ClubCard isStamped={true}></ClubCard>
-                    <ClubCard isFavorite={true} isStamped={true}></ClubCard>
-                    <ClubCard ></ClubCard>
-                    <ClubCard isStamped={true}></ClubCard>
-                    <ClubCard isFavorite={true} isStamped={true}></ClubCard>
+                    <ClubCard currentMode={currentMode}></ClubCard>
+                    <ClubCard isStamped={true} currentMode={currentMode}></ClubCard>
+                    <ClubCard isFavorite={true} isStamped={true} currentMode={currentMode}></ClubCard>
+                    <ClubCard currentMode={currentMode}></ClubCard>
+                    <ClubCard isStamped={true} currentMode={currentMode}></ClubCard>
+                    <ClubCard isFavorite={true} isStamped={true} currentMode={currentMode}></ClubCard>
+                    <ClubCard currentMode={currentMode}></ClubCard>
+                    <ClubCard isStamped={true} currentMode={currentMode}></ClubCard>
+                    <ClubCard isFavorite={true} isStamped={true} currentMode={currentMode}></ClubCard>
+                    <ClubCard currentMode={currentMode}></ClubCard>
+                    <ClubCard isStamped={true} currentMode={currentMode}></ClubCard>
+                    <ClubCard isFavorite={true} isStamped={true} currentMode={currentMode}></ClubCard>
+                    <ClubCard currentMode={currentMode}></ClubCard>
+                    <ClubCard isStamped={true} currentMode={currentMode}></ClubCard>
+                    <ClubCard isFavorite={true} isStamped={true} currentMode={currentMode}></ClubCard>
+                    <ClubCard currentMode={currentMode}></ClubCard>
+                    <ClubCard isStamped={true} currentMode={currentMode}></ClubCard>
+                    <ClubCard isFavorite={true} isStamped={true} currentMode={currentMode}></ClubCard>
                 </div>
             </div>
 
@@ -90,16 +84,19 @@ const Club = () => {
             <div className='fixed bottom-0 bg-gradient-to-t from-black/80 via-black/80 w-screen h-[25%]'></div>
 
             {/* button */}
-            <div className='fixed right-[15%] bottom-[8%] bg-white rounded-[20px] flex flex-row items-center justify-center px-[18px] py-[11px] gap-[15px]'>
-                <AiFillHeart className='w-[30px] h-[30px] cursor-pointer' color={favoriteColor} onClick={toggleFavorite}/>
+            <div className='fixed right-[15%] bottom-[8%] bg-white rounded-[28px] flex flex-row items-center justify-center px-[18px] py-[11px] gap-[15px]'>
+                {
+                    (currentMode == "Stamp") ? <FaStamp className='w-[30px] h-[30px] cursor-pointer' color='#9642E2' onClick={handleChangeMode} />
+                        : <AiFillHeart className='w-[30px] h-[30px] cursor-pointer' color='red' onClick={handleChangeMode} />
+                }
 
-                <div className='bg-[#D9D9D9] w-[2px] h-[30px]'/>
+                <div className='bg-[#D9D9D9] w-[2px] h-[30px]' />
 
-                <div className='relative cursor-pointer' onClick={toggleStamp}>
-                    <FaStamp className='relative w-[30px] h-[30px] cursor-pointer' color={stampColor} />
-                    {stampSlash} 
+                <div className='relative cursor-pointer' onClick={handleFilter}>
+                    <FaFilter className='relative w-[30px] h-[30px] cursor-pointer' color={filterColor} />
+                    {filterSlash}
                 </div>
-                
+
             </div>
         </div>
     );
