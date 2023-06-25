8c209da2a65bceaec8634fffe4996f8f816d423e
kikinnicha <kinatnicha@gmail.com>
2023-06-25 18:01:33 +0700

    feat: ui assets package

--- apps/links/src/pages/index.tsx
+++ apps/links/src/pages/index.tsx
@@ -1,7 +1,22 @@
 import type { NextPage } from 'next';
+import { vol1 } from 'assets';
+import Image from 'next/image';
 
 const Home: NextPage = () => {
-    return <main>links here</main>;
+    return (
+        <main>
+            <div className="relative w-80 h-screen">
+                <Image
+                    src={vol1.leaf1}
+                    alt="test"
+                    fill
+                    style={{
+                        objectFit: 'contain',
+                    }}
+                />
+            </div>
+        </main>
+    );
 };
 
 export default Home;
