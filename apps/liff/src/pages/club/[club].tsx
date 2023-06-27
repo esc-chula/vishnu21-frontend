1d4611c7fbbba571e9b1d0f1afad199072368bd7
Zenith <sqoulo11@gmail.com>
2023-06-27 14:51:05 +0700

    feat: return button

--- apps/liff/src/pages/club/[club].tsx
+++ apps/liff/src/pages/club/[club].tsx
@@ -1,6 +1,7 @@
 import { useRouter } from 'next/router';
 import * as React from 'react';
 import { useState, useEffect } from 'react';
+import { IoArrowBackCircle } from 'react-icons/io5'
 
 const ClubDetail = (() => {
     const router = useRouter();
@@ -8,6 +9,9 @@ const ClubDetail = (() => {
     return <div className='flex flex-col'>
         <h1>{router.query.name}</h1>
         <h1>{router.query.description}</h1>
+        <IoArrowBackCircle onClick={
+            () => router.push('/club')
+        } />
     </div>
 })
 
