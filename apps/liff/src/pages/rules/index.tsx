1be86c983c015f993d6958601dc6fc0a87b4fd35
unknown <natouchf40@gmail.com>
2023-06-28 12:51:04 +0700

    first commit(not done yet)

---
+++ apps/liff/src/pages/rules/index.tsx
@@ -0,0 +1,131 @@
+import type { NextPage } from 'next';
+import Image from 'next/image';
+import GrainBackground from 'images/GrainBackground.svg';
+import DecorateVector from 'images/DecorateVector.svg';
+import RuleItem from './rule';
+// import Shadow from 'images/ShadowBottom.svg';
+
+const allRule = [
+    {
+        id: '1',
+        topic: 'Lorem ipsum dolor sit amet consectetur?',
+        detail: 'Lorem ipsum dolor sit amet consectetur. Amet quam id netus et lorem. Nibh et sit eleifend scelerisque duis convallis tristique. Nibh et sit eleifend scelerisque duis convallis tristique.',
+    },
+    {
+        id: '2',
+        topic: 'Lorem ipsum dolor sit amet consectetur?',
+        detail: 'Lorem ipsum dolor sit amet consectetur. Amet quam id netus et lorem. Nibh et sit eleifend scelerisque duis convallis tristique. Nibh et sit eleifend scelerisque duis convallis tristique.',
+    },
+    {
+        id: '3',
+        topic: 'Lorem ipsum dolor sit amet consectetur?',
+        detail: 'Lorem ipsum dolor sit amet consectetur. Amet quam id netus et lorem. Nibh et sit eleifend scelerisque duis convallis tristique. Nibh et sit eleifend scelerisque duis convallis tristique.',
+    },
+    {
+        id: '4',
+        topic: 'Lorem ipsum dolor sit amet consectetur?',
+        detail: 'Lorem ipsum dolor sit amet consectetur. Amet quam id netus et lorem. Nibh et sit eleifend scelerisque duis convallis tristique. Nibh et sit eleifend scelerisque duis convallis tristique.',
+    },
+    {
+        id: '5',
+        topic: 'What is Lorem Ipsum?',
+        detail: 'Lorem ipsum dolor sit amet consectetur. Amet quam id netus et lorem. Nibh et sit eleifend scelerisque duis convallis tristique. Nibh et sit eleifend scelerisque duis convallis tristique.',
+    },
+    {
+        id: '6',
+        topic: 'What is Lorem Ipsum?',
+        detail: 'Lorem ipsum dolor sit amet consectetur. Amet quam id netus et lorem. Nibh et sit eleifend scelerisque duis convallis tristique. Nibh et sit eleifend scelerisque duis convallis tristique.',
+    },    {
+        id: '7',
+        topic: 'What is Lorem Ipsum?',
+        detail: 'Lorem ipsum dolor sit amet consectetur. Amet quam id netus et lorem. Nibh et sit eleifend scelerisque duis convallis tristique. Nibh et sit eleifend scelerisque duis convallis tristique.',
+    },    {
+        id: '8',
+        topic: 'What is Lorem Ipsum?',
+        detail: 'Lorem ipsum dolor sit amet consectetur. Amet quam id netus et lorem. Nibh et sit eleifend scelerisque duis convallis tristique. Nibh et sit eleifend scelerisque duis convallis tristique.',
+    },    {
+        id: '9',
+        topic: 'What is Lorem Ipsum?',
+        detail: 'Lorem ipsum dolor sit amet consectetur. Amet quam id netus et lorem. Nibh et sit eleifend scelerisque duis convallis tristique. Nibh et sit eleifend scelerisque duis convallis tristique.',
+    },    {
+        id: '10',
+        topic: 'Lorem ipsum dolor sit amet consectetur?',
+        detail: 'Lorem ipsum dolor sit amet consectetur. Amet quam id netus et lorem. Nibh et sit eleifend scelerisque duis convallis tristique. Nibh et sit eleifend scelerisque duis convallis tristique.',
+    },
+    {
+        id: '11',
+        topic: 'Lorem ipsum dolor sit amet consectetur?',
+        detail: 'Lorem ipsum dolor sit amet consectetur. Amet quam id netus et lorem. Nibh et sit eleifend scelerisque duis convallis tristique. Nibh et sit eleifend scelerisque duis convallis tristique.',
+    },
+    {
+        id: '12',
+        topic: 'Lorem ipsum dolor sit amet consectetur?',
+        detail: 'Lorem ipsum dolor sit amet consectetur. Amet quam id netus et lorem. Nibh et sit eleifend scelerisque duis convallis tristique. Nibh et sit eleifend scelerisque duis convallis tristique.',
+    },
+    {
+        id: '13',
+        topic: 'Lorem ipsum dolor sit amet consectetur?',
+        detail: 'Lorem ipsum dolor sit amet consectetur. Amet quam id netus et lorem. Nibh et sit eleifend scelerisque duis convallis tristique. Nibh et sit eleifend scelerisque duis convallis tristique.',
+    },
+    {
+        id: '14',
+        topic: 'What is Lorem Ipsum?',
+        detail: 'Lorem ipsum dolor sit amet consectetur. Amet quam id netus et lorem. Nibh et sit eleifend scelerisque duis convallis tristique. Nibh et sit eleifend scelerisque duis convallis tristique.',
+    },
+    {
+        id: '15',
+        topic: 'What is Lorem Ipsum?',
+        detail: 'Lorem ipsum dolor sit amet consectetur. Amet quam id netus et lorem. Nibh et sit eleifend scelerisque duis convallis tristique. Nibh et sit eleifend scelerisque duis convallis tristique.',
+    },    {
+        id: '16',
+        topic: 'What is Lorem Ipsum?',
+        detail: 'Lorem ipsum dolor sit amet consectetur. Amet quam id netus et lorem. Nibh et sit eleifend scelerisque duis convallis tristique. Nibh et sit eleifend scelerisque duis convallis tristique.',
+    },    {
+        id: '17',
+        topic: 'What is Lorem Ipsum?',
+        detail: 'Lorem ipsum dolor sit amet consectetur. Amet quam id netus et lorem. Nibh et sit eleifend scelerisque duis convallis tristique. Nibh et sit eleifend scelerisque duis convallis tristique.',
+    },    {
+        id: '18',
+        topic: 'What is Lorem Ipsum?',
+        detail: 'Lorem ipsum dolor sit amet consectetur. Amet quam id netus et lorem. Nibh et sit eleifend scelerisque duis convallis tristique. Nibh et sit eleifend scelerisque duis convallis tristique.',
+    },
+];
+
+const Rules: NextPage = () => {
+    return (
+        <div className="w-screen min-h-screen flex flex-col items-center px-7 py-16">
+            <h1 className="font-semibold text-white text-xl font-baijam -z-200">
+                กฎค่ายวิษณุครั้งที่ 21
+            </h1>
+            <div className="w-344px h-570px top-233 left-25 gap-11">
+                <ul className="w-full mt-6 flex items-center flex-col gap-5 font-ibm">
+                    {allRule.map((allRule) => (
+                        <RuleItem
+                        key={allRule.id}
+                        topic={allRule.topic}
+                        detail={allRule.detail}
+                    />
+                    ))}
+                </ul>
+                <ul className='w-full h-10 m-7'>
+                </ul>
+            </div>
+            <div className="fixed inset-x-0 bottom-0 z-[-10] h-40 bg-gradient-to-t from-black" />
+            <Image
+                src={GrainBackground}
+                alt="Background"
+                className="fixed top-0 left-0 -z-50 h-screen w-full object-cover"
+                loading="lazy"
+            />
+            <Image
+                src={DecorateVector}
+                alt=""
+                className="fixed bottom-0 left-0 -z-5 w-full"
+                loading="lazy"
+            />
+            
+        </div>
+    );
+};
+
+export default Rules;
---
+++ apps/liff/src/pages/rules/rule.tsx
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+// import { AiOutlineDown, AiOutlineRight } from 'react-icons/ai';
+type Rules = {
+    topic: string;
+    detail: string;
+};
+
+const RuleItem: React.FC<Rules> = ({topic , detail }) => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggle = () => setIsOpen((prev) => !prev);
+
+    return (
+        <li
+            className="w-full rounded-2xl p-2 shadow-details bg-gray-300 bg-opacity-30 text-white z-10"
+            onClick={toggle}
+        >
+            <div className="flex items-center justify-between cursor-pointer">
+                <h1 className="font-semibold text-sm pt-2 pr-5 pb-2 pl-1">{topic}</h1>
+                {/* {isOpen ? (
+                    <AiOutlineRight className="w-3 h-3 text-white" />
+                ) : (
+                    <AiOutlineDown className="w-3 h-3 text-" />
+                )} */}
+            </div>
+            <p
+                className={`pl-5 pr-8 overflow-hidden transition-all duration-300 ease-in-out text-xs leading-5 font-semibold ${
+                    isOpen ? 'max-h-96 mb-4' : 'max-h-0'
+                }`}
+            >
+                {detail}
+            </p>
+        </li>
+    );
+};
+
+export default RuleItem;
