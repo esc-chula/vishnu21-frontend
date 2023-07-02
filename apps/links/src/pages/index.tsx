28c8657122978cfea6ab4ffbddf140def22f82f4
kikinnicha <kinatnicha@gmail.com>
2023-07-02 22:42:55 +0700

    chore: add firstdate and vishnu logo to assets package

--- apps/links/src/pages/index.tsx
+++ apps/links/src/pages/index.tsx
@@ -1,9 +1,17 @@
 import Layout from '@/components/Layout';
+import { logo } from 'assets';
 import type { NextPage } from 'next';
+import Image from 'next/image';
 
 const Home: NextPage = () => {
     return (
         <Layout>
+            <Image
+                src={logo.firstdate}
+                alt="Vishnu Logo"
+                width={100}
+                height={100}
+            />
             <h1>hello world</h1>
         </Layout>
     );
