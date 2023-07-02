160ae69c03ad6c159577a6d707ab3b437e10f091
kikinnicha <kinatnicha@gmail.com>
2023-07-02 22:37:59 +0700

    chore: create layout to wrap everything

--- apps/links/src/pages/index.tsx
+++ apps/links/src/pages/index.tsx
@@ -1,15 +1,11 @@
-import Background from '@/components/Background';
+import Layout from '@/components/Layout';
 import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
     return (
-        <>
-            <div className="z-10 absolute top-0 left-0 right-0 bottom-0 overflow-y-auto">
-                <div className="text-white h-screen">fdfs</div>
-                <div className="text-white h-screen">fdfs</div>
-            </div>
-            <Background />
-        </>
+        <Layout>
+            <h1>hello world</h1>
+        </Layout>
     );
 };
 
