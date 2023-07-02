3078f64c046ab8e839a8583c1d260181f9d9cae7
kikinnicha <kinatnicha@gmail.com>
2023-07-02 23:38:34 +0700

    feat: links body

--- apps/links/src/pages/_document.tsx
+++ apps/links/src/pages/_document.tsx
@@ -18,7 +18,21 @@ class MyDocument extends Document {
     render() {
         return (
             <Html className="bg-black">
-                <Head></Head>
+                <Head>
+                    <link
+                        rel="preconnect"
+                        href="https://fonts.googleapis.com"
+                    />
+                    <link
+                        rel="preconnect"
+                        href="https://fonts.gstatic.com"
+                        crossOrigin="true"
+                    />
+                    <link
+                        href="https://fonts.googleapis.com/css2?family=Bai+Jamjuree:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;1,200;1,300;1,400;1,500;1,600;1,700&family=IBM+Plex+Sans+Thai+Looped:wght@100;200;300;400;500;600;700&display=swap"
+                        rel="stylesheet"
+                    />
+                </Head>
                 <body>
                     <Main />
                     <NextScript />
--- apps/links/src/pages/index.tsx
+++ apps/links/src/pages/index.tsx
@@ -2,17 +2,58 @@ import Layout from '@/components/Layout';
 import { logo } from 'assets';
 import type { NextPage } from 'next';
 import Image from 'next/image';
+import Link from 'next/link';
 
 const Home: NextPage = () => {
     return (
         <Layout>
-            <Image
-                src={logo.firstdate}
-                alt="Vishnu Logo"
-                width={100}
-                height={100}
-            />
-            <h1>hello world</h1>
+            <div className="flex flex-col items-center pt-28 space-y-20">
+                {/* informations */}
+                <div className="flex flex-col items-center space-y-5">
+                    {/* icon */}
+                    <div className="flex items-center space-x-3">
+                        <Image
+                            src={logo.firstdate}
+                            alt="First Date Logo"
+                            width={62}
+                            height={47}
+                        />
+                        <div className="h-12 w-px bg-white" />
+                        <Image
+                            src={logo.vishnu}
+                            alt="Vishnu Logo"
+                            width={138}
+                            height={38}
+                        />
+                    </div>
+
+                    {/* information */}
+                    <div className="text-center space-y-1">
+                        <h1 className="font-bold text-xl">@vishnu21st</h1>
+                        <h2 className="font-semibold text-sm">
+                            งาน Intania First Date 2023
+                            <br />
+                            และค่ายวิษณุกรรมบุตรครั้งที่ 21
+                        </h2>
+                    </div>
+                </div>
+
+                {/* links */}
+                <div className="flex flex-col w-80 space-y-10">
+                    <Link
+                        href="/"
+                        className="w-full h-12 bg-white rounded-2xl text-primary-900 flex items-center px-4 font-semibold shadow-button hover:scale-105 duration-300 ease-in-out"
+                    >
+                        LINE OA
+                    </Link>
+                    <Link
+                        href="/"
+                        className="w-full h-12 bg-white rounded-2xl text-primary-900 flex items-center px-4 font-semibold shadow-button hover:scale-105 duration-300 ease-in-out"
+                    >
+                        ฟอร์มสอบถามความต้องการเข้าร่วมงาน
+                    </Link>
+                </div>
+            </div>
         </Layout>
     );
 };
