08891dc6e8192bd7168432755ad7659e37c4b50f
Ebonian <tanadon.santisan@gmail.com>
2023-07-04 20:57:29 +0700

    chore: tap with hammer js

--- apps/web/src/components/game/siilued-mhoo/TapButtons.tsx
+++ apps/web/src/components/game/siilued-mhoo/TapButtons.tsx
@@ -1,5 +1,6 @@
 import { KeyPressContext } from '@/contexts/game/siilued-mhoo/KeyPressContext';
 import React, { useContext } from 'react';
+import ReactHammer from 'react-hammerjs';
 
 const TapButtons: React.FC = () => {
     const { keysPress, setKeysPress } = useContext(KeyPressContext);
@@ -7,32 +8,47 @@ const TapButtons: React.FC = () => {
     return (
         <div className="z-30 absolute bottom-0 left-0 right-0 border-t-[6px] border-white grid grid-cols-4">
             {['d', 'f', 'j', 'k'].map((key) => (
-                <button
+                <ReactHammer
                     key={key}
-                    onTouchStart={() => {
+                    onTap={() => {
+                        setKeysPress((prev) => ({
+                            ...prev,
+                            [key]: true,
+                        }));
+                        setTimeout(() => {
+                            setKeysPress((prev) => ({
+                                ...prev,
+                                [key]: false,
+                            }));
+                        }, 100);
+                    }}
+                    onPress={() => {
                         setKeysPress((prev) => ({
                             ...prev,
                             [key]: true,
                         }));
                     }}
-                    onTouchEnd={() => {
+                    onPressUp={() => {
                         setKeysPress((prev) => ({
                             ...prev,
                             [key]: false,
                         }));
                     }}
-                    className={`${
-                        keysPress[key]
-                            ? 'bg-black/[35%]'
-                            : `bg-black/50 active:bg-black/[35%]`
-                    } relative h-36 border border-white/5 group backdrop-blur-md`}
                 >
-                    <div
+                    <button
                         className={`${
-                            keysPress[key] ? 'h-80' : 'group-active:h-80'
-                        } absolute bottom-[150px] w-full bg-gradient-to-t from-white/40 to-transparent`}
-                    ></div>
-                </button>
+                            keysPress[key]
+                                ? 'bg-black/[35%]'
+                                : `bg-black/50 active:bg-black/[35%]`
+                        } relative h-36 border border-white/5 group backdrop-blur-md`}
+                    >
+                        <div
+                            className={`${
+                                keysPress[key] ? 'h-80' : 'group-active:h-80'
+                            } absolute bottom-[150px] w-full bg-gradient-to-t from-white/40 to-transparent`}
+                        ></div>
+                    </button>
+                </ReactHammer>
             ))}
         </div>
     );
--- apps/web/src/pages/game/index.tsx
+++ apps/web/src/pages/game/index.tsx
@@ -1,18 +1,7 @@
 import type { NextPage } from 'next';
-import Hammer from 'react-hammerjs';
 
 const Game: NextPage = () => {
-    return (
-        <div>
-            <Hammer
-                onTap={(e) => {
-                    console.log(e);
-                }}
-            >
-                <button>tap</button>
-            </Hammer>
-        </div>
-    );
+    return <div></div>;
 };
 
 export default Game;
