53e6dacda145ab30da1e5943c87e6df8db10415c
Ebonian <tanadon.santisan@gmail.com>
2023-07-04 19:34:08 +0700

    chore: add hammer js

--- apps/web/src/pages/game/index.tsx
+++ apps/web/src/pages/game/index.tsx
@@ -1,7 +1,18 @@
 import type { NextPage } from 'next';
+import Hammer from 'react-hammerjs';
 
 const Game: NextPage = () => {
-    return <div>Game</div>;
+    return (
+        <div>
+            <Hammer
+                onTap={(e) => {
+                    console.log(e);
+                }}
+            >
+                <button>tap</button>
+            </Hammer>
+        </div>
+    );
 };
 
 export default Game;
