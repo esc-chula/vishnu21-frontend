992bd885f3e25fab86307351115b33e283c7467d
Ebonian <tanadon.santisan@gmail.com>
2023-07-08 17:43:26 +0700

    feat: hero and overview section

---
+++ apps/web/src/components/landing/Decoration.tsx
@@ -0,0 +1,114 @@
+import Cloud1 from '@/public/images/landing/cloud1.png';
+import { ParallaxLayer } from '@react-spring/parallax';
+import Image from 'next/image';
+
+const Decoration: React.FC = () => {
+    return (
+        <>
+            {/* sun */}
+            <>
+                <ParallaxLayer className="-z-10" offset={0} speed={0.2}>
+                    <div
+                        className="absolute -bottom-36 sm:-bottom-64 -left-12 sm:-left-28 w-[240px] sm:w-[400px] aspect-square bg-[#f5ecda] bg-opacity-50 rounded-full"
+                        style={{
+                            filter: 'drop-shadow(0px 0px 200px #D9AB9F) blur(32px)',
+                        }}
+                    ></div>
+                </ParallaxLayer>
+                <ParallaxLayer className="-z-30" offset={0} speed={0.2}>
+                    <div
+                        className="absolute -bottom-36 sm:-bottom-64 -left-12 sm:-left-28 w-[240px] sm:w-[400px] aspect-square bg-[#ffffff] bg-opacity-70 rounded-full"
+                        style={{
+                            filter: 'drop-shadow(0px 0px 200px #D9AB9F) blur(32px)',
+                        }}
+                    ></div>
+                </ParallaxLayer>
+            </>
+            {/* cloud */}
+            <>
+                {/* large cloud */}
+                <ParallaxLayer className="-z-20" offset={0} speed={0.4}>
+                    <div className="absolute w-[700px] sm:w-[1400px] aspect-[1863/845] -bottom-16 sm:-bottom-[240px] -right-64 sm:-right-[440px] select-none">
+                        <div className="relative w-full h-full">
+                            <Image
+                                src={Cloud1}
+                                alt="Cloud"
+                                fill
+                                className="object-contain"
+                            />
+                        </div>
+                    </div>
+                </ParallaxLayer>
+                {/* small cloud */}
+                <ParallaxLayer
+                    className="-z-20 hidden sm:inline"
+                    offset={0}
+                    speed={0.2}
+                >
+                    <div
+                        className="absolute w-[840px] aspect-[1863/845] -bottom-20 -left-72 select-none"
+                        style={{
+                            transform: 'scaleX(-1)',
+                        }}
+                    >
+                        <div className="relative w-full h-full">
+                            <Image
+                                src={Cloud1}
+                                alt="Cloud"
+                                fill
+                                className="object-contain"
+                            />
+                        </div>
+                    </div>
+                </ParallaxLayer>
+            </>
+            {/* background */}
+            <>
+                {/* star background */}
+                <ParallaxLayer className="-z-40" offset={0} factor={3}>
+                    <svg
+                        className="h-full w-full"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <filter className="h-full w-full" id="filter">
+                            <feTurbulence baseFrequency="0.2" />
+                            <feColorMatrix
+                                values="0 0 0 8 -4
+                   0 0 0 8 -4
+                   0 0 0 8 -4
+                   0 0 0 8 -4"
+                            />
+                        </filter>
+                        <rect
+                            width="100%"
+                            height="100%"
+                            filter="url(#filter)"
+                        />
+                    </svg>
+                </ParallaxLayer>
+                {/* gradient background */}
+                <ParallaxLayer
+                    className="-z-50"
+                    offset={0}
+                    factor={1}
+                    style={{
+                        backgroundImage:
+                            'linear-gradient(180deg, #000 0%, #4713A3 86.82%)',
+                    }}
+                ></ParallaxLayer>
+                {/* gradient background */}
+                <ParallaxLayer
+                    className="-z-50"
+                    offset={1}
+                    factor={2}
+                    style={{
+                        backgroundImage:
+                            'linear-gradient(0deg, #1E084A 40.55%, #4713A3 88.16%)',
+                    }}
+                ></ParallaxLayer>
+            </>
+        </>
+    );
+};
+
+export default Decoration;
--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,168 +1,142 @@
-import Cloud1 from '@/public/images/cloud1.png';
-import Divider1 from '@/public/images/divider1.svg';
+import Logo from '@/components/landing/Logo';
+import Divider from '@/components/landing/Divider';
+import Image1 from '@/public/images/landing/1.webp';
+import Image2 from '@/public/images/landing/2.webp';
+import Image3 from '@/public/images/landing/3.webp';
+import Image4 from '@/public/images/landing/4.webp';
+import Image5 from '@/public/images/landing/5.webp';
+import Image6 from '@/public/images/landing/6.webp';
+import { Parallax, ParallaxLayer } from '@react-spring/parallax';
 import type { NextPage } from 'next';
 import Image from 'next/image';
-import { FiChevronDown } from 'react-icons/fi';
+import Navigation from '@/components/landing/Navigation';
+import Decoration from '@/components/landing/Decoration';
+import { useState } from 'react';
+import NavigationDrawer from '@/components/landing/NavigationDrawer';
 
 const Home: NextPage = () => {
-    return (
-        <main className="overflow-hidden min-h-screen w-full">
-            <section
-                className="w-full h-screen relative grid place-content-center"
-                style={{
-                    backgroundImage:
-                        'linear-gradient(180deg, #000 0%, #4713A3 86.82%)',
-                }}
-            >
-                {/* scroll button */}
-                <div className="z-50 absolute bottom-10 left-0 right-0 flex justify-center text-3xl">
-                    <FiChevronDown className="cursor-pointer" />
-                </div>
+    const [toggleNav, setToggleNav] = useState(false);
 
-                {/* text */}
-                <div className="z-40 absolute w-full h-full flex flex-col justify-center items-center pb-20 space-y-4 sm:space-y-6 text-center">
-                    <h1 className="flex flex-col items-center font-bold space-y-4">
-                        <span
-                            className="text-sm sm:text-2xl"
-                            style={{
-                                textShadow:
-                                    '0px 4px 17px rgba(71, 19, 163, 0.75)',
-                            }}
-                        >
-                            ยินดีต้อนรับน้อง ๆ ทุกคนเข้าสู่ค่าย
-                        </span>
-                        <span
-                            className="text-4xl sm:text-7xl"
-                            style={{
-                                textShadow:
-                                    '0px -41px 140px rgba(255, 231, 106, 1), 0px 7px 30px rgba(71, 19, 163, 0.75)',
-                            }}
-                        >
-                            ค่ายวิษณุกรรมบุตร
-                        </span>
-                    </h1>
-                    <div className="flex flex-col space-y-8 sm:space-y-10 w-full">
-                        <div className="relative w-full h-[7px] sm:h-[14px] select-none">
-                            <Image src={Divider1} alt="Divider" fill />
-                        </div>
-                        <div className="flex flex-col items-center space-y-10 sm:space-y-12">
-                            <p className="font-baijam font-semibold text-sm sm:text-2xl flex flex-col space-y-3">
-                                <span>
-                                    น้อง ๆ สามารถติดตามกิจกรรมและข้อมูลอื่น ๆ
-                                </span>
-                                <span>ได้ผ่าน LINE OA ลิงก์ด้านล่างนี้เลย</span>
-                            </p>
-                            <button
-                                className="bg-white text-primary-600 font-bold sm:text-xl px-6 sm:px-8 py-2.5 sm:py-3 rounded-2xl duration-300 hover:scale-105"
+    return (
+        <>
+            <Navigation setToggleNav={setToggleNav} />
+            {toggleNav && <NavigationDrawer setToggleNav={setToggleNav} />}
+
+            <Parallax pages={3}>
+                {/* hero */}
+                <ParallaxLayer offset={0} speed={0.3}>
+                    <div className="z-40 absolute w-full h-full flex flex-col justify-center items-center pb-20 space-y-4 sm:space-y-6 text-center">
+                        <h1 className="flex flex-col items-center font-bold space-y-4">
+                            <span
+                                className="text-sm sm:text-2xl"
                                 style={{
-                                    boxShadow:
-                                        '0px 4.536645889282227px 21px 0px rgba(255, 231, 106, 0.65), 0px -4px 8px 0px rgba(194, 150, 239, 0.42) inset',
+                                    textShadow:
+                                        '0px 4px 17px rgba(71, 19, 163, 0.75)',
                                 }}
                             >
-                                ADD LINE
-                            </button>
+                                ยินดีต้อนรับน้อง ๆ ทุกคนเข้าสู่ค่าย
+                            </span>
+                            <span
+                                className="text-4xl sm:text-7xl"
+                                style={{
+                                    textShadow:
+                                        '0px -41px 140px rgba(255, 231, 106, 1), 0px 7px 30px rgba(71, 19, 163, 1)',
+                                }}
+                            >
+                                ค่ายวิษณุกรรมบุตร
+                            </span>
+                        </h1>
+                        <div className="flex flex-col items-center space-y-8 sm:space-y-10 w-full">
+                            <div className="relative w-[270px] sm:w-[560px]">
+                                <Divider />
+                            </div>
+
+                            {/* <div className="relative aspect-[742/12] w-[800px] select-none">
+                                <Image src={Divider1} alt="Divider" fill />
+                            </div> */}
+                            <div className="flex flex-col items-center space-y-10 sm:space-y-12">
+                                <p className="font-baijam font-semibold text-sm sm:text-2xl flex flex-col space-y-3">
+                                    <span>
+                                        น้อง ๆ สามารถติดตามกิจกรรมและข้อมูลอื่น
+                                        ๆ
+                                    </span>
+                                    <span>
+                                        ได้ผ่าน LINE OA ลิงก์ด้านล่างนี้เลย
+                                    </span>
+                                </p>
+                                <button
+                                    className="bg-white text-primary-600 font-bold sm:text-xl px-6 sm:px-8 py-2.5 sm:py-3 rounded-2xl duration-300 hover:scale-105"
+                                    style={{
+                                        boxShadow:
+                                            '0px 4.536645889282227px 21px 0px rgba(255, 231, 106, 0.65), 0px -4px 8px 0px rgba(194, 150, 239, 0.42) inset',
+                                    }}
+                                >
+                                    ADD LINE
+                                </button>
+                            </div>
                         </div>
                     </div>
-                </div>
+                    <div className="z-30 absolute w-full h-full flex justify-center items-center">
+                        <div
+                            className="max-w-[1000px] w-full aspect-square rounded-full"
+                            style={{
+                                background:
+                                    'radial-gradient(50% 50.00% at 50% 50.00%, rgba(255, 231, 106, 0.14) 38.54%, rgba(255, 231, 106, 0.00) 100%)',
+                            }}
+                        ></div>
+                    </div>
+                </ParallaxLayer>
 
-                {/* front sun */}
-                <div
-                    className="z-30 absolute -bottom-80 -left-28 w-[400px] aspect-square bg-[#F0D396] bg-opacity-50 rounded-full mix-blend-screen"
-                    style={{
-                        filter: 'drop-shadow(0px 0px 200px #D9AB9F) blur(32px)',
-                    }}
-                ></div>
-                <div
-                    className="z-0 absolute -bottom-80 -left-28 w-[400px] aspect-square bg-[#F0D396] bg-opacity-50 rounded-full mix-blend-screen"
-                    style={{
-                        filter: 'drop-shadow(0px 0px 200px #D9AB9F) blur(32px)',
-                    }}
-                ></div>
-
-                {/* back sun */}
-
-                {/* text background */}
-                <div className="z-30 absolute w-full h-full flex justify-center items-center">
-                    <div
-                        className="max-w-[1000px] w-full aspect-square rounded-full"
-                        style={{
-                            background:
-                                'radial-gradient(50% 50.00% at 50% 50.00%, rgba(255, 231, 106, 0.14) 38.54%, rgba(255, 231, 106, 0.00) 100%)',
-                        }}
-                    ></div>
-                </div>
-
-                {/* large cloud */}
-                <div className="z-20 absolute w-[1400px] aspect-[1863/845] -bottom-[280px] -right-[440px] select-none">
-                    <div className="relative w-full h-full">
+                {/* camp overview */}
+                <ParallaxLayer
+                    offset={1}
+                    factor={0.5}
+                    speed={0.5}
+                    className="flex flex-col justify-center items-center space-y-10 sm:space-y-16 px-4"
+                >
+                    <Logo className="sm:scale-110" />
+                    <p className="font-baijam font-semibold text-lg sm:text-2xl text-center text-primary-50 max-w-[807px] leading-loose tracking-normal sm:leading-loose sm:tracking-wide">
+                        ค่ายวิษณุกรรมบุตร
+                        เป็นค่ายที่จัดเพื่อต้อนรับนิสิตใหม่ของคณะวิศวฯ จุฬาฯ
+                        ซึ่งได้จัดติดต่อกันมาเป็นเวลากว่า 20 ปีแล้ว !
+                        ค่ายนี้จะทำให้ทุกคนได้รู้จัก
+                        และสร้างความสนิทสนมกับเพื่อน ๆ ชาววิศวฯ
+                        ผ่านกิจกรรมสุดจัดเต็มตลอด 4 วัน
+                    </p>
+                </ParallaxLayer>
+                <ParallaxLayer
+                    offset={1.5}
+                    factor={0.3}
+                    speed={0.6}
+                    className="flex justify-center px-4 select-none"
+                >
+                    <div className="grid grid-cols-2 gap-4 sm:gap-10 max-w-screen-lg w-full">
                         <Image
-                            src={Cloud1}
-                            alt="Cloud"
-                            fill
-                            className="object-contain"
+                            src={Image5}
+                            alt="Camp Photo"
+                            className="object-contain scale-90 rounded-2xl shadow-2xl"
+                        />
+                        <Image
+                            src={Image1}
+                            alt="Camp Photo"
+                            className="object-contain scale-95 rounded-2xl shadow-2xl"
+                        />
+                        <Image
+                            src={Image3}
+                            alt="Camp Photo"
+                            className="object-contain scale-100 rounded-2xl shadow-2xl"
+                        />
+                        <Image
+                            src={Image6}
+                            alt="Camp Photo"
+                            className="object-contain scale-90 rounded-2xl shadow-2xl"
                         />
                     </div>
-                </div>
+                </ParallaxLayer>
 
-                {/* small cloud */}
-                <div
-                    className="z-10 absolute w-[840px] aspect-[1863/845] -bottom-32 -left-72 select-none"
-                    style={{
-                        transform: 'scaleX(-1)',
-                    }}
-                >
-                    <div className="relative w-full h-full">
-                        <Image
-                            src={Cloud1}
-                            alt="Cloud"
-                            fill
-                            className="object-contain"
-                        />
-                    </div>
-                </div>
-
-                {/* star background */}
-                <svg
-                    className="z-0 absolute flex h-full w-full"
-                    xmlns="http://www.w3.org/2000/svg"
-                >
-                    <filter className="h-full w-full" id="filter">
-                        <feTurbulence baseFrequency="0.2" />
-                        <feColorMatrix
-                            values="0 0 0 8 -4
-                               0 0 0 8 -4
-                               0 0 0 8 -4
-                               0 0 0 8 -4"
-                        />
-                    </filter>
-                    <rect width="100%" height="100%" filter="url(#filter)" />
-                </svg>
-            </section>
-            <section
-                className="w-full h-[200vh]"
-                style={{
-                    backgroundImage:
-                        'linear-gradient(0deg, #1E084A 40.55%, #4713A3 88.16%)',
-                }}
-            >
-                {/* star background */}
-                <svg
-                    className="flex h-full w-full"
-                    xmlns="http://www.w3.org/2000/svg"
-                >
-                    <filter className="h-full w-full" id="filter">
-                        <feTurbulence baseFrequency="0.2" />
-                        <feColorMatrix
-                            values="0 0 0 8 -4
-                               0 0 0 8 -4
-                               0 0 0 8 -4
-                               0 0 0 8 -4"
-                        />
-                    </filter>
-                    <rect width="100%" height="100%" filter="url(#filter)" />
-                </svg>
-            </section>
-        </main>
+                <Decoration />
+            </Parallax>
+        </>
     );
 };
 
