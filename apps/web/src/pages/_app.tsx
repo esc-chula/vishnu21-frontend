aa3bc0b1fdba4b42ed8a936239af76d55c09ec09
Ebonian <tanadon.santisan@gmail.com>
2023-07-19 19:07:04 +0700

    feat: analytics

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -4,10 +4,16 @@ import 'ui/styles.css';
 
 import type { AppProps } from 'next/app';
 import { WEB_PRODUCTION } from 'env';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
     if (WEB_PRODUCTION) {
-        return <Component {...pageProps} />;
+        return (
+            <>
+                <Component {...pageProps} />;
+                <Analytics />
+            </>
+        );
     } else {
         return <></>;
     }
