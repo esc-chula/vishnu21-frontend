2d6310ce8da8a1e72ba3ac7a92e3381b2fdccaa7
Ebonian <tanadon.santisan@gmail.com>
2023-07-20 21:01:57 +0700

    chore: edit post and preview

--- apps/admin/src/components/GroupHomePageMock.tsx
+++ apps/admin/src/components/GroupHomePageMock.tsx
@@ -7,8 +7,8 @@ interface GroupHomePageMockProps {
 const GroupHomePageMock: React.FC<GroupHomePageMockProps> = ({ children }) => {
     return (
         <div className="aspect-[393/852] relative">
-            <div className="absolute left-0 right-0 bottom-0 top-[25.5%] bg-neutral-400 z-10 overflow-y-auto space-y-10">
-                {children}
+            <div className="absolute left-0 right-0 bottom-0 top-[25.5%] bg-neutral-400 z-10 overflow-hidden space-y-10">
+                <div className="relative w-full h-full">{children}</div>
             </div>
             <GroupHomePageBackground />
         </div>
