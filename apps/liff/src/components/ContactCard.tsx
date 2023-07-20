7a9385a8cb3de158c8f9aa13ee66bc3f18429505
Ebonian <tanadon.santisan@gmail.com>
2023-07-20 17:59:09 +0700

    feat: contact page

--- apps/liff/src/components/ContactCard.tsx
+++ apps/liff/src/components/ContactCard.tsx
@@ -1,8 +1,39 @@
-interface ContactCardProps {}
+import { FiPhone } from 'react-icons/fi';
 
-const ContactCard: React.FC<ContactCardProps> = ({}) => {
+interface ContactCardProps {
+    name: string;
+    department: string;
+    tel: string;
+    line: string;
+}
+
+const ContactCard: React.FC<ContactCardProps> = ({
+    name,
+    department,
+    tel,
+    line,
+}) => {
     return (
-        <div className="bg-neutral-50 shadow-button w-full h-16 rounded-2xl mb-4 cursor-pointer"></div>
+        <a
+            href={`tel:${tel.replace(/-/g, '')}`}
+            className="bg-neutral-50 shadow-button w-full rounded-2xl mb-4 cursor-pointer text-neutral-900 py-4 px-5 flex justify-between items-center"
+        >
+            <div>
+                <h4 className="font-semibold mb-1">
+                    {name}
+                    <span className="text-sm text-gray-500 px-2">{`(${department})`}</span>
+                </h4>
+                <div className="flex text-sm">
+                    <div className="w-12">เบอร์</div>
+                    <div>{tel}</div>
+                </div>
+                <div className="flex text-sm">
+                    <div className="w-12">ไลน์</div>
+                    <div>{line}</div>
+                </div>
+            </div>
+            <FiPhone className="text-xl mr-1 text-primary-600" />
+        </a>
     );
 };
 
--- apps/liff/src/pages/contact.tsx
+++ apps/liff/src/pages/contact.tsx
@@ -24,27 +24,37 @@ const Contact: NextPage = () => {
             </Head>
             <Main foregroundImage="full2">
                 <h1 className="font-bold text-2xl mt-12">ติดต่อฉุกเฉิน</h1>
-                <div className="flex flex-col items-center w-full h-full px-10 mt-6">
+                <div className="flex flex-col items-center w-full h-full overflow-y-auto pb-20 px-10 mt-6">
                     {houseData && (
                         <div className="flex flex-col items-center w-full">
-                            <h3 className="font-medium text-sm opacity-60 mb-4">
+                            <h3 className="font-medium text-sm opacity-70 mb-4">
                                 พี่บ้าน{houseData.shortName}
                             </h3>
-                            <ContactCard />
-                            <ContactCard />
+                            <ContactCard name="" department="" tel="" line="" />
+                            <ContactCard name="" department="" tel="" line="" />
                         </div>
                     )}
                     <div className="flex flex-col items-center w-full">
-                        <h3 className="font-medium text-sm opacity-60 mb-4">
+                        <h3 className="font-medium text-sm opacity-70 mb-4">
                             ฝ่ายพยาบาล
                         </h3>
-                        <ContactCard />
+                        <ContactCard
+                            name="พี่โฟล์ค"
+                            department="พยาบาลกลาง"
+                            tel="095-945-9928"
+                            line="folkieszarama"
+                        />
                     </div>
                     <div className="flex flex-col items-center w-full">
-                        <h3 className="font-medium text-sm opacity-60 mb-4">
+                        <h3 className="font-medium text-sm opacity-70 mb-4">
                             ฝ่ายทะเบียน
                         </h3>
-                        <ContactCard />
+                        <ContactCard
+                            name="พี่มาร์ค"
+                            department="ทะเบียนกลาง"
+                            tel="086-868-8242"
+                            line="086-868-8242"
+                        />
                     </div>
                 </div>
             </Main>
